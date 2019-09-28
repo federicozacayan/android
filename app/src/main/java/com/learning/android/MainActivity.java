e027818e8893b44a478809bb49fae981e588a00b
Federico Zacayan <federico.zacayan@gmail.com>
2019-09-28 16:40:36 +1200

    This is a single notification

--- app/src/main/java/com/learning/android/MainActivity.java
+++ app/src/main/java/com/learning/android/MainActivity.java
@@ -1,7 +1,9 @@
 package com.learning.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This method NotificationCompat.Builder is deprecated
+        NotificationCompat.Builder nb = new NotificationCompat.Builder(this);
+        nb.setContentText("This is the message");
+        nb.setContentTitle("This is the title");
+        nb.setSmallIcon(R.mipmap.ic_launcher);
+
+        NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(1, nb.build());
     }
 }
