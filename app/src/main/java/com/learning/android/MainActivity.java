8504c0fcee441d8df67ba9680a575f6e9cc75dd7
Federico Zacayan <federico.zacayan@gmail.com>
2019-09-30 00:14:49 +1300

    Notification 2.0

--- app/src/main/java/com/learning/android/MainActivity.java
+++ app/src/main/java/com/learning/android/MainActivity.java
@@ -1,14 +1,54 @@
 package com.learning.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static String TAG = "Notifier";
+    public static final String CHANNEL_ID = "exampleServiceChannel";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+
+
+        Log.d(TAG, "luchNotification");
+        NotificationCompat.Builder nb = new NotificationCompat.Builder(this);
+        Intent resutlIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 1, resutlIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        nb.setContentText("This is the text");
+        nb.setContentTitle("This is the Title");
+        nb.setSmallIcon(R.mipmap.ic_launcher);
+        nb.setAutoCancel(true);
+        nb.setContentIntent(resultPendingIntent);
+
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(1, nb.build());
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Example Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+
+        }
     }
 }
